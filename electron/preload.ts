94d6a7458b3367ac64e498e001bcb207b14d6d07
arkadii <arkadii.personal@proton.me>
2026-04-24 12:30:07 +0200

    typical stupid mokey

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,4 +2,5 @@ import { ipcRenderer, contextBridge } from "electron";
 
 contextBridge.exposeInMainWorld("api", {
   sendCommand: (text: string) => ipcRenderer.invoke("send-command", text),
+  getTelemetry: () => ipcRenderer.invoke("get-telemetry"),
 });
